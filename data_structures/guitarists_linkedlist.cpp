3beb30ae8e9daca2435587e870253334b70d1e15
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-09-15 10:37:06 -0400

    Refactor

--- data_structures/guitarists_linkedlist.cpp
+++ data_structures/guitarists_linkedlist.cpp
@@ -14,22 +14,54 @@ public:
     // Fields
     string nickName;
     string guitar;
-    
+
     // Address of the next node
     GuitaristNode* next;
-    
+
     // Constructor
     GuitaristNode() {
         next = nullptr; // 0
     }
 };
 
+// Create a linkedlist of gutarists
+GuitaristNode* createGuitarists() {
+    GuitaristNode* head;
+    GuitaristNode* currentNode;
+
+    // Create first node
+    currentNode = new GuitaristNode;
+    currentNode->nickName = "Yngwie Malmsteen";
+    currentNode->guitar   = "Stratocaster";
+
+    // Give the address to the previous node
+    head = currentNode;
+
+    // Create second node
+    currentNode = new GuitaristNode;
+    currentNode->nickName = "Michael Schenker";
+    currentNode->guitar   = "Flying V";
+
+    // Give the address to the previous node
+    head->next = currentNode;
+
+    // Create third node
+    currentNode = new GuitaristNode;
+    currentNode->nickName = "Marty Friedmann";
+    currentNode->guitar   = "Some nice guitar";
+
+    // Give the address to the previous node
+    head->next->next = currentNode;
+
+    return head;
+}
+
 // Print a linked list from head to tail
 void printGuitarists(GuitaristNode* head) {
-    
+
     // Set the cursor to the head
     GuitaristNode* currentNode = head;
-    
+
     // Print the entire list
     while (currentNode != nullptr) {
         cout << "Guitarist: "
@@ -41,55 +73,32 @@ void printGuitarists(GuitaristNode* head) {
     }
 }
 
-
 int main() {
     GuitaristNode* head;
-    GuitaristNode* currentNode;
     GuitaristNode* temp;
-    
+
     //==> Creating a list
-    
-    // Create first node
-    currentNode = new GuitaristNode;
-    currentNode->nickName = "Yngwie Malmsteen";
-    currentNode->guitar   = "Stratocaster";
-    
-    // Give the address to the previous node
-    head = currentNode;
-    
-    // Create second node
-    currentNode = new GuitaristNode;
-    currentNode->nickName = "Michael Schenker";
-    currentNode->guitar   = "Flying V";
-    
-    // Give the address to the previous node
-    head->next = currentNode;
-    
-    // Create third node
-    currentNode = new GuitaristNode;
-    currentNode->nickName = "Marty Friedmann";
-    currentNode->guitar   = "Some nice guitar";
-    
-    // Give the address to the previous node
-    head->next->next = currentNode;
-    
+
+    head = createGuitarists();
+
     // Print the entire list
     printGuitarists(head);
+
     cout << "--------------------------------------------------------" << endl;
-    
+
     //==> Deleting the first node
-    
+
     // Remember the address of the new head
     temp = head->next;
-    
+
     // Delete the node from the memory
     delete head;
-    
+
     // Set the head to the new address
     head = temp;
-    
+
     // Print the entire list
     printGuitarists(head);
-    
+
     return 0;
 }
